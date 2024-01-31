7035111da93231fddb79a6e12b2cc105acf454f3
Алексей Петренко <aipetrenko@corp.finam.ru>
2024-01-31 19:54:05 +0600

    Remove unused

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,47 @@
 
 
 plugins {
-	`java-gradle-plugin`
-	kotlin("jvm") version "1.9.22"
-	id("com.gradle.plugin-publish") version "1.2.1"
-	id("nu.studer.credentials") version "3.0"
+    `java-gradle-plugin`
+    kotlin("jvm") version "1.9.22"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "io.github.aipwebdev"
 version = "1.0"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 publishing {
-	repositories {
-		maven {
-			name = "localPluginRepository"
-			url = uri("../local-plugin-repository")
-		}
-	}
+    repositories {
+        maven {
+            name = "localPluginRepository"
+            url = uri("../local-plugin-repository")
+        }
+    }
 }
 
 dependencies {
-	testImplementation("org.assertj:assertj-core:3.9.0")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.assertj:assertj-core:3.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 gradlePlugin {
-	website = "https://github.com/aip-webdev/gradle-in-docker-teamcity-plugin"
-	vcsUrl = "https://github.com/aip-webdev/gradle-in-docker-teamcity-plugin.git"
-	plugins {
-		create("gradle-in-docker-ci") {
-			id = "io.github.aipwebdev.gradle-in-docker-ci"
-			implementationClass = "io.github.aipwebdev.DockerInGradleTeamcityPlugin"
-			displayName = "Plugin to produce test output as Teamcity Service Messages with retry"
-			description = "Produces tests' output in Teamcity Service Message format with retry"
-			tags = listOf("teamcity", "service messages", "docker", "test output", "retry")
-		}
-	}
+    website = "https://github.com/aip-webdev/gradle-in-docker-teamcity-plugin"
+    vcsUrl = "https://github.com/aip-webdev/gradle-in-docker-teamcity-plugin.git"
+    plugins {
+        create("gradle-in-docker-ci") {
+            id = "io.github.aipwebdev.gradle-in-docker-ci"
+            implementationClass = "io.github.aipwebdev.DockerInGradleTeamcityPlugin"
+            displayName = "Plugin to produce test output as Teamcity Service Messages with retry"
+            description = "Produces tests' output in Teamcity Service Message format with retry"
+            tags = listOf("teamcity", "service messages", "docker", "test output", "retry")
+        }
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
 rootProject.name = "gradle-in-docker-teamcity-plugin"
-
-// enableFeaturePreview("STABLE_PUBLISHING")
